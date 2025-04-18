ac1ed130c9a22aeb0adf5c94e3e878d445ca85bc
mango2309 <sebastian.largo@udla.edu.ec>
2025-04-17 22:53:36 -0500

    Creacion del Manager para manejo de puntaje

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] private float initialVelocity = 4f;
+    [SerializeField] private float velocityMultiplier = 1.1f;
 
     private Rigidbody2D ballRb;
 
@@ -21,8 +22,28 @@ public class Ball : MonoBehaviour
         ballRb.velocity = new Vector2(xVelocity, yVelocity) * initialVelocity;
     }
 
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            ballRb.velocity *= velocityMultiplier;
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Goal1"))
+        {
+            GameManager.Instance.Paddle2Scored();
+            GameManager.Instance.Restart();
+            Launch();
+        }
+        else
+        {
+            GameManager.Instance.Paddle1Scored();
+            GameManager.Instance.Restart();
+            Launch();
+        }
+    }
+
 }
